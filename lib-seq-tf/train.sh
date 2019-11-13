47a638265fabc5cd890f5e65ef9d18ccd3dbb48f
Achyudh Ram <achyudhk@gmail.com>
2019-11-12 22:48:08 -0500

    Add code2seq author code

--- lib-seq-tf/train.sh
+++ lib-seq-tf/train.sh
@@ -1,4 +1,3 @@
-#!/usr/bin/env bash
 ###########################################################
 # Change the following values to train a new model.
 # type: the name of the new model, only affects the saved file name.
@@ -6,13 +5,13 @@
 # test_data: by default, points to the validation set, since this is the set that
 #   will be evaluated after each training iteration. If you wish to test
 #   on the final (held-out) test set, change 'val' to 'test'.
-type=java-small
-dataset_name=java-small
-data_dir=../../precis-data/${dataset_name}
+type=java-large-model
+dataset_name=java-large
+data_dir=data/java-large
 data=${data_dir}/${dataset_name}
-test_data=${data_dir}/${dataset_name}.val.c2v
+test_data=${data_dir}/${dataset_name}.val.c2s
 model_dir=models/${type}
 
-mkdir -p models/${model_dir}
+mkdir -p ${model_dir}
 set -e
-python3 -u code2vec.py --data ${data} --test ${test_data} --save ${model_dir}/saved_model
+python3 -u code2seq.py --data ${data} --test ${test_data} --save_prefix ${model_dir}/model
