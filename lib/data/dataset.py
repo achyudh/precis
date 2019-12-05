Fix masking and related bugs in the code2seq model

lib/data/dataset.py
@@ -1,10 +1,9 @@
 from argparse import Namespace
 from enum import Enum
 
-import torch
 from torch.utils.data import Dataset
 
-from lib.data.reader import PathContextReader, PathContextInput
+from lib.data.reader import PathContextReader
 
 
 class DatasetSplit(Enum):
@@ -38,7 +37,7 @@ class JavaSummarizationDataset(Dataset):
                     self.input_tensors.append(input_tensor)
                     self.target_labels.append(target_label)
 
-    def __getitem__(self, idx: int) -> PathContextInput:
+    def __getitem__(self, idx: int):
         return self.input_tensors[idx]
 
     def __len__(self) -> int:

lib/data/reader.py
@@ -1,38 +1,12 @@
 import abc
 from argparse import Namespace
-from typing import NamedTuple, Tuple
+from typing import Tuple
 
 import numpy as np
 import torch
 
+from lib.data import PathContextInput, SequentialPathContextInput
 from lib.data.vocab import Code2VecVocabContainer, Code2SeqVocabContainer
-
-
-class PathContextInput(NamedTuple):
-    """
-    Used mostly for convenient-and-clear access to input parts (by their names).
-    """
-    path_indices: torch.Tensor
-    source_token_indices: torch.Tensor
-    target_token_indices: torch.Tensor
-    context_valid_mask: torch.Tensor
-    target_index: torch.Tensor
-    sample_index: torch.Tensor
-
-
-class SequentialPathContextInput(NamedTuple):
-    """
-    Used mostly for convenient-and-clear access to input parts (by their names).
-    """
-    node_indices: torch.Tensor
-    node_lengths: torch.Tensor
-    source_subtoken_indices: torch.Tensor
-    source_subtoken_lengths: torch.Tensor
-    target_subtoken_indices: torch.Tensor
-    target_subtoken_lengths: torch.Tensor
-    context_valid_mask: torch.Tensor
-    target_indices: torch.Tensor
-    sample_index: torch.Tensor
 
 
 class AbstractContextReader(abc.ABC):
@@ -113,6 +87,7 @@ class SequentialPathContextReader(AbstractContextReader):
 
     def _map_raw_dataset_row_to_input_tensors(self, index, *row_parts) -> Tuple:
         row_parts = list(row_parts)
+
         subtoken_pad_string = self.vocab.subtoken_vocab.special_words.PAD
         subtoken_pad_index = self.vocab.subtoken_vocab.word_to_index[subtoken_pad_string]
         node_pad_string = self.vocab.node_vocab.special_words.PAD
@@ -145,14 +120,14 @@ class SequentialPathContextReader(AbstractContextReader):
 
         node_strings = [x[1].split('|') for x in split_contexts][:self.config.max_path_nodes]
         node_lengths = [len(string) for string in node_strings]
-        node_lengths += [0 for _ in range(self.config.max_contexts - len(node_lengths))]
+        node_lengths += [1 for _ in range(self.config.max_contexts - len(node_lengths))]
         node_lengths = torch.tensor(node_lengths)
 
         node_indices = [[self.vocab.node_vocab.lookup_index(x) for x in string] for string in node_strings]
         node_indices = torch.tensor(self._pad_sequence(node_indices, pad_value=node_pad_index,
                                     shape=(self.config.max_contexts, self.config.max_path_nodes)))
 
-        target_subtoken_strings = [x[1].split('|') for x in split_contexts][:self.config.max_subtokens]
+        target_subtoken_strings = [x[2].split('|') for x in split_contexts][:self.config.max_subtokens]
         target_subtoken_lengths = [len(string) for string in target_subtoken_strings]
         target_subtoken_lengths += [0 for _ in range(self.config.max_contexts - len(target_subtoken_strings))]
         target_subtoken_lengths = torch.tensor(target_subtoken_lengths)

lib/model/code2seq/decoder.py
@@ -32,8 +32,10 @@ class ContextDecoder(nn.Module):
     def init_input(self, batch_size):
         return torch.tensor([self.target_sos_index] * batch_size, device=self.config.device)  # (batch,)
 
-    def init_hidden(self, contexts, context_valid_mask, batch_size):
-        context_sum = torch.sum(contexts * torch.unsqueeze(context_valid_mask, -1), dim=1)  # (batch, decoder_hidden_dim)
-        h_0 = context_sum / self.config.max_contexts
-        c_0 = torch.zeros((batch_size, self.config.decoder_hidden_dim), device=self.config.device)
+    def init_state(self, contexts, context_valid_mask, batch_size):
+        context_sum = torch.sum(contexts, dim=1)  # (batch, decoder_hidden_dim)
+        context_counts = torch.sum(context_valid_mask, dim=1).unsqueeze(-1)  # (batch,)
+
+        c_0 = context_sum / context_counts  # (batch, decoder_hidden_dim)
+        h_0 = torch.zeros((batch_size, self.config.decoder_hidden_dim), device=self.config.device)
         return h_0, c_0

lib/model/code2seq/encoder.py
@@ -1,6 +1,5 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 
 
 class NodeEncoder(nn.Module):
@@ -10,10 +9,8 @@ class NodeEncoder(nn.Module):
         self.lstm = nn.LSTM(config.node_embedding_dim, config.encoder_hidden_dim, dropout=config.dropout_rate,
                             bidirectional=True, batch_first=True)
 
-    def forward(self, node_embed, node_lengths, context_valid_mask):
-        context_valid_mask_flat = torch.reshape(context_valid_mask, shape=(-1,))  # (batch * max_contexts)
-        node_lengths = torch.reshape(node_lengths, (-1,)) * context_valid_mask_flat.long()  # (batch * max_contexts)
-        node_lengths[node_lengths == 0] = 1
+    def forward(self, node_embed, node_lengths):
+        node_lengths = torch.reshape(node_lengths, (-1,))  # (batch * max_contexts)
 
         # (batch * max_contexts, max_path_nodes, node_embedding_dim)
         x = torch.reshape(node_embed, shape=(-1, self.config.max_path_nodes, self.config.node_embedding_dim))